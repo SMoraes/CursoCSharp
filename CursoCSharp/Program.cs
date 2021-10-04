a4978caee0fdcfd19cedc0b96dc9503eafdf588f
Sergio Moraes <sergio.tec1@hotmail.com>
2021-10-04 18:11:00 -0300

    Primeiro Programa

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro curso");
+        }
+    }
+}

29298283aefb869abb67b9cc15ebd15c70220ebc
Sergio Moraes <sergio.tec1@hotmail.com>
2021-11-01 12:06:44 -0300

    Operadores de Atribuicao

---
+++ CursoCSharp/Fundamentos/OperadoresAtribuicao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresAtribuicao
+    {
+        public static void Executar()
+        {
+            var num1 = 3;
+            num1 = 7;
+            num1 += 10; // num1 = num1 + 10;
+            num1 -= 3; // num1 = num1 - 3;
+            num1 *= 5; // num1 = num1 * 5;
+            num1 /= 2; // num1 = num1 / 2;
+
+            Console.WriteLine(num1);
+
+            // Atributo de valor 
+            int a = 1;
+            int b = a;
+
+            a++; // a = a + 1;
+            b--; // b = b - 1;
+
+            Console.WriteLine($"{a} {b}");
+
+            // Não se preocupe com o código
+            // Atributo de Referência.
+            dynamic c = new System.Dynamic.ExpandoObject();
+            c.nome = "Sergio";
+
+            dynamic d = c;
+            d.nome = "Adriano";
+
+            Console.WriteLine(c.nome);
+
+        }
+    }
+}

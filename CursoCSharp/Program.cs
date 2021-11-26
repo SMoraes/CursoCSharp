02be5a5f22c0c3103ac37699b90b9dc625dd5720
Sergio Moraes <sergio.tec1@hotmail.com>
2021-11-26 15:44:07 -0300

    Parametros Nomeados

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -48,6 +48,7 @@ namespace CursoCSharp {
                 {"Atributos Estaticos - Classes e Métodos", AtributosEstaticos.Executar},
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
                 {"Params - Classes e Métodos", Params.Executar},
+                {"ParametrosNomeados - Classes e Métodos", ParametrosNomeados.Executar},
 
             });
 

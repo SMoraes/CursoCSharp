08b1da901acf57eace7835ea7eecd8c1c9787784
Sergio Moraes <sergio.tec1@hotmail.com>
2021-12-03 08:23:43 -0300

    Classe e Metodos - Get e Set

--- CursoCSharp/ClassesEMetodos/GetSet.cs
+++ CursoCSharp/ClassesEMetodos/GetSet.cs
@@ -10,9 +10,9 @@ namespace CursoCSharp.ClassesEMetodos
     {
         private string Marca;
         private string Modelo;
-        private int Cilindrada;
+        private uint Cilindrada;
 
-        public Moto(string marca, string modelo, int cilindrada)
+        public Moto(string marca, string modelo, uint cilindrada)
         {
             //Marca = marca;
             //Modelo = modelo;
@@ -43,24 +43,28 @@ namespace CursoCSharp.ClassesEMetodos
             Modelo = modelo;
         }
 
-        public int GetCilindrada()
+        public uint GetCilindrada()
         {
             return Cilindrada;
         }
-        public void SetCilindrada(int cilindrada)
+        public void SetCilindrada(uint cilindrada)
         {
+            // 1 Opção
             //if (cilindrada > 0)
             //{
             //    Cilindrada = cilindrada;
             //}
-            Cilindrada = Math.Abs(cilindrada);
+            // 2 Opção
+            //Cilindrada = Math.Abs(cilindrada);
+
+            Cilindrada = cilindrada;
         }
     }
     class GetSet
     {
         public static void Executar()
         {
-            var moto1 = new Moto("Kawasaki", "Ninja ZX-6R", -636);
+            var moto1 = new Moto("Kawasaki", "Ninja ZX-6R", 636);
 
             Console.WriteLine(moto1.GetMarca());
             Console.WriteLine(moto1.GetModelo());
@@ -69,7 +73,7 @@ namespace CursoCSharp.ClassesEMetodos
             var moto2 = new Moto();
             moto2.SetMarca("Honda");
             moto2.SetModelo("CG Titan");
-            moto2.SetCilindrada(-150);
+            moto2.SetCilindrada(150);
 
             Console.WriteLine(moto2.GetMarca() + " " + moto2.GetModelo()
                 + " " + moto2.GetCilindrada());

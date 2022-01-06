e363eae7e88ff363e59b56fdfc00d33e069d8b25
Sergio Moraes <sergio.tec1@hotmail.com>
2022-01-06 18:24:13 -0300

    Colecoes

--- CursoCSharp/Colecoes/ColecoesList.cs
+++ CursoCSharp/Colecoes/ColecoesList.cs
@@ -24,6 +24,10 @@ namespace CursoCSharp.Colecoes
             bool mesmoPreco = Preco == outroProduto.Preco;
             return mesmoNome && mesmoPreco;
         }
+        public override int GetHashCode()
+        {
+            return Nome.Length;
+        }
     }
     class ColecoesList
     {
